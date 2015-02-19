2ddf9fa5ced527168d7d1331c5bd28225ee2abbe
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-19 11:45:34 -0500

    adds `first` property

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * parse-copyright <https://github.com/jonschlinkert/parse-copyright>
  *
- * Copyright (c) 2015 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
@@ -42,11 +42,20 @@ function parse(str, options) {
   if (match[2]) { res.symbol = match[2]; }
   // '2013-2015, 2016, '
   if (match[3]) { res.dateRange = clean(match[3]); }
+  if (match[3]) { res.first = first(res.dateRange); }
   // '2016'
   if (match[4]) { res.latest = match[4]; }
   // 'Jon Schlinkert'
   if (match[5]) { res.author = clean(match[5]); }
   return res;
+}
+
+function first(str) {
+  var years = str.split(/\W/);
+  if (years && years.length && /^\d+$/.test(years[0])) {
+    return years[0];
+  }
+  return null;
 }
 
 function clean(str) {
